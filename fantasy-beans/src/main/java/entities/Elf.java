3a82c532faaba3cb665a02b16e898e739625cfb0
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-06-16 16:11:02 +0200

    Content negotiation on 'GET /forests'

--- fantasy-beans/src/main/java/entities/Elf.java
+++ fantasy-beans/src/main/java/entities/Elf.java
@@ -3,15 +3,16 @@ package entities;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-@Data
+@XmlRootElement
 @NoArgsConstructor
 @AllArgsConstructor
 @NamedQuery(
@@ -53,5 +54,78 @@ public class Elf implements Serializable {
     this.bowType = bowType;
     this.powerType = powerType;
     this.forest = forest;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getArrows() {
+    return arrows;
+  }
+
+  public void setArrows(int arrows) {
+    this.arrows = arrows;
+  }
+
+  public BowType getBowType() {
+    return bowType;
+  }
+
+  public void setBowType(BowType bowType) {
+    this.bowType = bowType;
+  }
+
+  public PowerType getPowerType() {
+    return powerType;
+  }
+
+  public void setPowerType(PowerType powerType) {
+    this.powerType = powerType;
+  }
+
+  @XmlTransient
+  public Forest getForest() {
+    return forest;
+  }
+
+  public void setForest(Forest forest) {
+    this.forest = forest;
+  }
+
+  public void setForestId(int forestId) {
+    this.forestId = forestId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Elf elf = (Elf) o;
+    return id == elf.id &&
+      arrows == elf.arrows &&
+      forestId == elf.forestId &&
+      Objects.equals(name, elf.name) &&
+      bowType == elf.bowType &&
+      powerType == elf.powerType &&
+      Objects.equals(forest, elf.forest);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(id, name, arrows, bowType, powerType, forest, forestId);
   }
 }
